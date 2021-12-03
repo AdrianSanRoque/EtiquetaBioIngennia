d2ab6dc1a7c1a5887d839e0b6b5548734912bd62
AdrianSanRoque <60479266+AdrianSanRoque@users.noreply.github.com>
2021-12-03 11:04:41 +0100

    Commit Base

---
+++ Agro.Tag.Core/ContractRepositories/ContractsGeneic/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agro.Tag.Core.ContractRepositories.ContractsGeneic
+{
+    public interface IGenericRepository<TEntity> where TEntity:class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity,bool>> expression);
+        TEntity Insert(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        TEntity Deleted(TEntity entity);
+    }
+}
---
+++ Agro.Tag.Core/Entities/ErrorEtiqueta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agro.Tag.Core.Entities
+{
+    
+    public class ErrorEtiqueta
+    {
+        public string ErrorEt { get; set; }
+    }
+}
