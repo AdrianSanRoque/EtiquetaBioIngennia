a2d53c98a1166701133a5d24506fa30905fbe767
AdrianSanRoque <60479266+AdrianSanRoque@users.noreply.github.com>
2021-12-03 11:05:37 +0100

    Commit Base

---
+++ Agro.Tag.EF.BD.Infrastructure/DAL/ApplicationDbContext.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Agro.Tag.Core.Entities;
+
+
+
+namespace Agro.Tag.EF.BD.Infrastructure.DAL
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ):base(options)
+        {
+            
+            /*
+            System.Configuration.LazyLoadingEnabled = false;
+            Database.Log = x => Trace.WriteLine(x);
+            */           
+        }
+
+        public DbSet<Core.Entities.Tag> Tag { get; set; }
+
+        /*
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            
+            OnBeforeSaving();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        */
+
+        /*
+        private void OnBeforeSaving()
+        {
+            var entries = ChangeTracker.Entries();
+            var utcNow = DateTime.UtcNow;
+
+            foreach (var entry in entries)
+            {
+                // for entities that inherit from BaseEntity,
+                // set UpdatedOn / CreatedOn appropriately
+                if (entry.Entity is BaseEntity trackable)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                            // set the updated date to "now"
+                            trackable.UpdatedAt = utcNow;
+                            // mark property as "don't touch"
+                            // we don't want to update on a Modify operation
+                            entry.Property("CreatedAt").IsModified = false;
+                            break;
+
+                        case EntityState.Added:
+                            // set both updated and created date to "now"
+                            trackable.CreatedAt = utcNow;
+                            trackable.UpdatedAt = utcNow;
+                            break;
+                    }
+                }
+            }
+        }
+
+      */
+
+        /*
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+            modelBuilder.Entity<Core.Entities.Tag>().HasKey(e => e.Id);
+           
+            base.OnModelCreating(modelBuilder);
+        }
+        */
+    }
+}
---
+++ Agro.Tag.EF.BD.Infrastructure/ImpContractRepository/TagRepository.cs
@@ -0,0 +1,81 @@
+﻿using Agro.Tag.Core.ContractRepositories.ContractsGeneic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agro.Tag.Core.Entities;
+using System.Linq.Expressions;
+using Agro.Tag.EF.BD.Infrastructure.DAL;
+
+namespace Agro.Tag.EF.BD.Infrastructure.ImpContractRepository
+{
+    public class TagRepository : IGenericRepository<Core.Entities.Tag>
+    {
+
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public TagRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public Core.Entities.Tag Deleted(Core.Entities.Tag entity)
+        {
+            
+            var dt = _applicationDbContext.Entry(entity).State;
+            if (_applicationDbContext.Entry(entity).State == Microsoft.EntityFrameworkCore.EntityState.Detached)
+            {
+
+                _applicationDbContext.Remove(entity);
+                _applicationDbContext.SaveChanges();
+
+            }
+            return entity;
+
+        }
+
+        public Core.Entities.Tag Get(int id)
+        {
+            var result = _applicationDbContext.Tag.FirstOrDefault(c => c.Id == id);
+            return result;
+        }
+
+        public IEnumerable<Core.Entities.Tag> Get(Expression<Func<Core.Entities.Tag, bool>> expression)
+        {
+            var list = _applicationDbContext.Tag.Where(expression).ToList();
+            return list;
+        }
+
+        public Core.Entities.Tag Insert(Core.Entities.Tag entity)
+        {
+            
+
+            var dt = _applicationDbContext.Entry(entity).State;
+
+            if (_applicationDbContext.Entry(entity).State == Microsoft.EntityFrameworkCore.EntityState.Detached)
+            {
+
+             _applicationDbContext.Add(entity);
+             _applicationDbContext.SaveChanges();
+
+            }
+            return entity;
+        }
+
+        public Core.Entities.Tag Update(Core.Entities.Tag entity)
+        {
+           
+
+            var dt = _applicationDbContext.Entry(entity).State;
+            if (_applicationDbContext.Entry(entity).State == Microsoft.EntityFrameworkCore.EntityState.Detached)
+            {
+                
+                _applicationDbContext.Update(entity);
+                _applicationDbContext.SaveChanges();
+            }
+            
+            return entity;
+        }
+    }
+}
